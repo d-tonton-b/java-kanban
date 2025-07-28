2f8405b3bf8bf004b769a11b9fbc5b856b931b73
d-tonton-b <dpopkab@yandex.ru>
2025-07-28 15:21:28 +0300

    Add files via upload

---
+++ src/test/ManagersTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import manager.HistoryManager;
+import manager.Managers;
+import manager.TaskManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagersTest {
+
+    @Test
+    public void shouldReturnInitialisedTasKManager(){
+        TaskManager manager = Managers.getDefault();
+        assertNotNull(manager, "Менеджер задач должен быть проинициализирован");
+    }
+
+    @Test
+    public void shouldReturnInitialisedHistoryManager(){
+        HistoryManager manager = Managers.getDefaultHistory();
+        assertNotNull(manager, "Менеджер истории должен быть проинициализирован");
+    }
+
+}
