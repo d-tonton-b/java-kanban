9aacacb2d628659e24c1fa8d98af123ff28653cf
d-tonton-b <dpopkab@yandex.ru>
2025-08-06 12:26:07 +0300

    Delete src directory

--- src/manager/ManagerException.java
+++
@@ -1,7 +0,0 @@
-package manager;
-
-public class ManagerException extends RuntimeException {
-    public ManagerException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
